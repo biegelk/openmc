2179527f0744ebc22e69e2d6bf87dd0d2648e8e2
Katie Biegel <biegelk@kilkenny.bw01.mit.edu>
2013-07-18 11:51:09 -0700

    deallocated temp_filters

--- src/input_xml.F90
+++ src/input_xml.F90
@@ -2028,28 +2028,18 @@ contains
             end if
           case ('prompt-fission')
             t % score_bins(j) = SCORE_PROMPT_FISSION
-            if (t % find_filter(FILTER_ENERGYOUT) > 0) then
-              message = "Cannot tally prompt fission rate with an &
-                   &outgoing energy filter."
-              call fatal_error()
-            end if
+            ! Set tally estimator to analog
             t % estimator = ESTIMATOR_ANALOG
           case ('delayed-fission')
             t % score_bins(j) = SCORE_DELAYED_FISSION
-            if (t % find_filter(FILTER_ENERGYOUT) > 0) then
-              message = "Cannot tally delayed fission rate with an &
-                   &outgoing energy filter."
-              call fatal_error()
-            end if
+            ! Set tally estimator to analog
             t % estimator = ESTIMATOR_ANALOG
             ! Need to add pg filter to t % filters
             call move_alloc(t % filters, temp_filters)
-            allocate(t % filters(t % n_filters + 1))
-            do p = 2, n_filters + 1
-              do q = 1, n_filters
-                t % filters(p) = temp_filters(q)
-              end do
-            end do
+            t % n_filters = t % n_filters + 1
+            allocate(t % filters(t % n_filters))
+            t % filters(2 : t % n_filters) = temp_filters
+            deallocate(temp_filters)
             t % filters(1) % type = FILTER_PRECURSOR_GROUP
             t % filters(1) % n_bins = 6
           case ('nu-fission')
